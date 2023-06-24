2637bb67860b756729d456dd8ad042ffc139e9d9
uy_sun <hmy0119@gmail.com>
2023-06-24 12:50:23 +0800

    补全另外两种

--- tests/plugins/echo.py
+++ tests/plugins/echo.py
@@ -1,12 +1,30 @@
 from nonebot import on_command
 from nonebot.adapters import Bot, Event
 
-from nonebot_plugin_userinfo import get_user_info
+from nonebot_plugin_userinfo import BotUserInfo, EventUserInfo, UserInfo, get_user_info
 
 user_info_cmd = on_command("user_info")
 
 
 @user_info_cmd.handle()
-async def handle(bot: Bot, event: Event):
+async def user_info_handle(bot: Bot, event: Event):
     user_info = await get_user_info(bot, event, event.get_user_id())
     await user_info_cmd.send("", user_info=user_info)
+
+
+user_info_depends_cmd = on_command("user_info_depends")
+
+
+@user_info_depends_cmd.handle()
+async def user_info_depends_handle(
+    user_info: UserInfo = EventUserInfo(),
+):
+    await user_info_depends_cmd.send("", user_info=user_info)
+
+
+bot_user_info_cmd = on_command("bot_user_info")
+
+
+@bot_user_info_cmd.handle()
+async def handle(user_info: UserInfo = BotUserInfo()):  # 获取Bot用户信息
+    await bot_user_info_cmd.send("", user_info=user_info)
--- tests/test_user_info.py
+++ tests/test_user_info.py
@@ -6,20 +6,20 @@ from nonebug import App
 
 
 @pytest.mark.asyncio
-async def test_get_plugin(app: App):
+async def test_user_info(app: App):
     from nonebot_plugin_userinfo import UserInfo
-    from tests.plugins.echo import user_info_cmd
+    from tests.plugins.echo import user_info_cmd, user_info_depends_cmd
 
     event = MessageEvent(
         time=datetime.now(),
         self_id="test",
         message=Message("/user_info"),
-        user=User(id="123456789"),
+        user=User(id="123456789", nickname="MyUser"),
     )
 
     user_info = UserInfo(
-        user_id="User",
-        user_name="User",
+        user_id="MyUser",
+        user_name="MyUser",
         user_displayname=None,
         user_remark=None,
         user_avatar=None,
@@ -27,6 +27,60 @@ async def test_get_plugin(app: App):
     )
 
     async with app.test_matcher(user_info_cmd) as ctx:
+        bot = ctx.create_bot(base=Bot)
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(event, "", True, user_info=user_info)
+
+
+@pytest.mark.asyncio
+async def test_user_info_depends(app: App):
+    from nonebot_plugin_userinfo import UserInfo
+    from tests.plugins.echo import user_info_depends_cmd
+
+    event = MessageEvent(
+        time=datetime.now(),
+        self_id="test",
+        message=Message("/user_info_depends"),
+        user=User(id="123456789", nickname="MyUser"),
+    )
+
+    user_info = UserInfo(
+        user_id="MyUser",
+        user_name="MyUser",
+        user_displayname=None,
+        user_remark=None,
+        user_avatar=None,
+        user_gender="unknown",
+    )
+
+    async with app.test_matcher(user_info_depends_cmd) as ctx:
+        bot = ctx.create_bot(base=Bot)
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(event, "", True, user_info=user_info)
+
+
+@pytest.mark.asyncio
+async def test_bot_user_info(app: App):
+    from nonebot_plugin_userinfo import UserInfo
+    from tests.plugins.echo import bot_user_info_cmd
+
+    event = MessageEvent(
+        time=datetime.now(),
+        self_id="test",
+        message=Message("/bot_user_info"),
+        user=User(id="123456789", nickname="MyUser"),
+    )
+
+    user_info = UserInfo(
+        user_id="test",
+        user_name="Bot",
+        user_displayname=None,
+        user_remark=None,
+        user_avatar=None,
+        user_gender="unknown",
+    )
+
+    async with app.test_matcher(bot_user_info_cmd) as ctx:
         bot = ctx.create_bot(base=Bot)
         ctx.receive_event(bot, event)
         ctx.should_call_send(event, "", True, user_info=user_info)
